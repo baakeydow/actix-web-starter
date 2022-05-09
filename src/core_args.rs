2f6c0d1a85ec1221d0dcad2dbc4f1e43983cdba1
baakeydow <baakey@21times2.com>
2022-05-09 20:01:53 +0200

    parse args into CoreArgs with clap

---
+++ src/core_args.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+
+/// ACTIX-WEB-STARTER
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct CoreArgs {
+    // Dev mode
+    #[clap(short, long)]
+    pub dev: bool,
+
+    /// Main application tick
+    #[clap(short, long, default_value_t = 10)]
+    pub cron_time: u64,
+
+    // Schedule tick
+    #[clap(short, long, default_value = "0 * * * * * *")]
+    pub sch_time: String,
+
+    /// Max count per protected endpoint
+    #[clap(short, long, default_value_t = 100)]
+    pub max_endpoint_count: u64,
+}
+
+
